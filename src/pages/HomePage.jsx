8cd9f12492bfb1aa297c27ccf942a2740b5b0902
isabellacgmsa <isabellacgmsa1@gmail.com>
2025-04-17 10:26:49 -0300

    home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,6 @@ export default function HomePage() {
     
       return (
         <Flex vertical align="center" gap={40} style={{ padding: 40 }}>
-          {/* Logo e Título */}
           <Flex vertical align="center" gap={10}>
             <img
               src="https://rei-da-derivada.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.f28d5551.png&w=384&q=75"
@@ -17,13 +16,11 @@ export default function HomePage() {
             <Title level={2}>Rei/Rainha da Derivada</Title>
           </Flex>
     
-          {/* Texto principal */}
           <Flex vertical align="center" gap={16}>
             <Title level={1}>ALGUM TEXTO AQUI</Title>
             <Button type="primary" onClick={() => navigate('/about')}>Saiba Mais</Button>
           </Flex>
     
-          {/* Cartões de Objetivo */}
           <Row gutter={16} justify="center">
             {[1, 2, 3].map((item) => (
               <Col key={item} xs={24} sm={12} md={6}>
@@ -38,7 +35,6 @@ export default function HomePage() {
             ))}
           </Row>
     
-          {/* Texto final e botão Google */}
           <Flex vertical align="center" gap={16}>
             <Paragraph style={{ fontSize: 18, textAlign: "center" }}>
               ALGO DO TIPO, PARA PARTICIPAR DO RRDD<br />
